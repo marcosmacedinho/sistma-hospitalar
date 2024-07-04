deb1fef6d2ef1f6e9b329ef164f20db9eac3d114
marcosmacedo <marcosmacedo784@gmail.com>
2024-07-04 20:31:37 -0300

    sistemaHospitalar

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,20 +1,19 @@
 // src/firebaseConfig.js
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDVnU3UtVTfyKQVxrdpWgwm7iLbS3F2Sw4",
+  authDomain: "hospital-1951d.firebaseapp.com",
+  projectId: "hospital-1951d",
+  storageBucket: "hospital-1951d.appspot.com",
+  messagingSenderId: "294600042958",
+  appId: "1:294600042958:web:47a0c5aac703110706639f"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
 
-export { db, auth };
+export { db, auth, firebaseApp };
