43452d326a961486e24fb1bd0ff1bd1af1853f67
marcosmacedo <marcosmacedo784@gmail.com>
2024-07-04 18:46:37 -0300

    sistemaHospitalar

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../components/login.vue';
 import Register from '../components/Register.vue';
-import Atendimento from '../components/Atendimento.vue';
 import VisualizarAtendimentos from '../components/VisualizarAtendimentos.vue';
+import Atendimento from '../components/Atendimento.vue';
+
 
 const routes = [
   { path: '/', redirect: '/login' },
